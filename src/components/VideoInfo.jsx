bb0fa2b95651a5a4e4095715625e5e9f2063519c
Daan Pronk <daanpronk570@gmail.com>
2026-03-23 09:31:20 +0100

    fetch

--- src/components/VideoInfo.jsx
+++ src/components/VideoInfo.jsx
@@ -3,7 +3,17 @@ import styles from './videoInfo.module.css';
 export function VideoInfo() {
   return (
     <div className={styles.videoInfo}>
-      Video Info Component
+      <div className="vide_title">
+        <p>
+        Monkey jumping on car
+        </p>
+      </div>
+      <div className="video_data">
+        channnel name
+      </div>
+      <div className="video_desc">
+        description
+      </div>
     </div>
   );
 };
